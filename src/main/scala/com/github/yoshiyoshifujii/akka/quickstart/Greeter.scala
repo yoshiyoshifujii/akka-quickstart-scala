df07620312a6f46c124de4b9497241de8ed1dcec
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2020-08-27 14:43:04 +0900

    add Greeter

---
+++ src/main/scala/com/github/yoshiyoshifujii/akka/quickstart/Greeter.scala
@@ -0,0 +1,16 @@
+package com.github.yoshiyoshifujii.akka.quickstart
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ ActorRef, Behavior }
+
+object Greeter {
+  final case class Greet(whom: String, replyTo: ActorRef[Greeted])
+  final case class Greeted(whom: String, replyTo: ActorRef[Greet])
+
+  def apply(): Behavior[Greet] =
+    Behaviors.receive { (context, message) =>
+      context.log.info("Hello {}!", message.whom)
+      message.replyTo ! Greeted(message.whom, context.self)
+      Behaviors.same
+    }
+}
